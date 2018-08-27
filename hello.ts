5cdbac2d2f12a4d257af4d2e597b0364b5ac4c0a
freewind <nowindlee@gmail.com>
2018-08-27 15:42:59 +0800

    demo

--- hello.ts
+++ hello.ts
@@ -1,1 +1,9 @@
-console.log("Hello, Typescript");
+const names = ['aaa', 'bbb', 'ccc']
+
+console.log(names)
+for (const index in names) {
+    console.log(`index: ${index}`)
+    console.log(`value: ${names[index]}`)
+    console.log(`typeof(index): ${typeof(index)}`)
+    console.log()
+}
---
+++ hello.ts.md
@@ -0,0 +1,15 @@
+```
+$ npx ts-node hello.ts
+[ 'aaa', 'bbb', 'ccc' ]
+index: 0
+value: aaa
+typeof(index): string
+
+index: 1
+value: bbb
+typeof(index): string
+
+index: 2
+value: ccc
+typeof(index): string
+```
